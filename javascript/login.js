ac45d64625254d12214f06ac7290684cce48ee05
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-26 00:03:50 +0300

    Adding signup form on html and popup using JS file

---
+++ javascript/login.js
@@ -0,0 +1,5 @@
+var signBtn = document.getElementById("signBtn");
+console.log("hi;");
+signBtn.addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex";
+});
