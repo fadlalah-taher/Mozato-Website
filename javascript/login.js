0a9434e8fac5942354d6f0206a0a9f7a638662ad
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-26 07:14:41 +0300

    Adding Css design(icon) and adding function on Js file

--- javascript/login.js
+++ javascript/login.js
@@ -1,5 +1,28 @@
 var signBtn = document.getElementById("signBtn");
-console.log("hi;");
+var hide1 = document.getElementById("hide1");
+var hide2 = document.getElementById("hide2");
+var passwordIcon = document.getElementById("password-icon");
+var inputPassword = document.getElementById("inputPassword");
+var popup = document.querySelector(".popup");
+
 signBtn.addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "flex";
+    document.querySelector(".popup").style.display = "block";
+});
+window.onclick = function(event) {
+    if (event.target == signBtn) {
+        signBtn.style.display = "none";
+    }
+}
+passwordIcon.addEventListener("click", function(){
+    if(inputPassword.type === 'password'){
+        inputPassword.type = "text";
+        hide1.style.display = "block";
+        hide2.style.display = "none";
+      }
+      else{
+        inputPassword.type = "password";
+        hide1.style.display = "none";
+        hide2.style.display = "block";
+
+      }
 });
