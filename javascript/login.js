566f34877c33e8575e48d262ef9fc89de249c74c
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-28 15:01:54 +0300

    Adding function on Js(login) that hide and unhide password

--- javascript/login.js
+++ javascript/login.js
@@ -2,8 +2,13 @@ var signBtn = document.getElementById("signBtn");
 var hide1 = document.getElementById("hide1");
 var hide2 = document.getElementById("hide2");
 var passwordIcon = document.getElementById("password-icon");
+var passwordRegister = document.getElementById("passwordRegister-icon");
 var inputPassword = document.getElementById("inputPassword");
+var inputRegister = document.getElementById("inputPassword-register");
 var popup = document.querySelector(".popup");
+
+
+// burger Menu
 
 signBtn.addEventListener("click", function(){
     popup.style.display = "flex";
@@ -15,6 +20,9 @@ window.onclick = function(event) {
         console.log("hello window none");
     }
 }
+
+// hidden password
+
 passwordIcon.addEventListener("click", function(){
     if(inputPassword.type === 'password'){
         inputPassword.type = "text";
@@ -28,3 +36,17 @@ passwordIcon.addEventListener("click", function(){
 
       }
 });
+passwordRegister.addEventListener("click", function(){
+    if(inputRegister.type === 'password'){
+        inputRegister.type = "text";
+        hide3.style.display = "block";
+        hide4.style.display = "none";
+      }
+      else{
+        inputRegister.type = "password";
+        hide3.style.display = "none";
+        hide4.style.display = "block";
+
+      }
+});
+
