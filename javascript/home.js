33fa45b38120fecd2c00ba5a47554a367b9c0da0
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-28 19:46:19 +0300

    Adding design on home.css(profile section) and editing profile.html

--- javascript/home.js
+++ javascript/home.js
@@ -3,8 +3,12 @@ var icon = document.getElementById("span-icon");
 console.log("helo");
 icon.addEventListener("click", function(){
     myNav.style.width = "100%";
-    console.log("hello word");
 });
 myNav.addEventListener("click", function(){
     myNav.style.width = "0%";
 });
+
+
+
+
+
