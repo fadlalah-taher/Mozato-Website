a8775fd2724c0f20ff21ca2f7da3f0392ac27372
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-29 12:14:56 +0300

    Adding profile.php (with Update query)

---
+++ php/profile.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include("connection.php");
+
+$fullname = $_POST["full_name"];
+$email = $_POST["email"];
+//$password = hash("sha256", $_POST["password"]);
+$phonenumber = $_POST["phone_number"];  
+$address = $_POST["address"];
+$gender = $_POST["gender"];
+$age = $_POST["age"];
+//$user_role = 0;
+
+$query = $mysqli->prepare("UPDATE users(full_name, email, password, phone_number, address, age) SET (?, ?, ?, ?, ?, ?)");
+$query->bind_param("sssssi", $fullname, $email, $password, $phonenumber, $address, $age);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
