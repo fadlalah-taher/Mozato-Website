7d73208f4b4877e5702d65ceac1aba69d7f67df3
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-29 09:45:23 +0300

    Adding users.php that will display the users

---
+++ php/users.php
@@ -0,0 +1,15 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+include("connection.php");
+$query = $mysqli->prepare("SELECT * from users");
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($todo = $array->fetch_assoc()){
+    $response[] = $todo;
+} 
+$json = json_encode($response);
+echo $json;
+
+?>
