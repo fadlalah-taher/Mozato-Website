601a90709ba701d1e9320b090da6f36c0bcfcb00
fadlalah-taher <fadeltaher111@gmail.com>
2022-05-31 09:17:15 +0300

    Cleaning profile.php code

--- php/profile.php
+++ php/profile.php
@@ -34,16 +34,8 @@ if($_POST["address"] == ""){
     die("mess s around");
 }else{
     $address = $_POST["address"]; 
-}/*
-$fullname = $_POST["full_name"];
-$email = $_POST["email"];
-$password = hash("sha256", $_POST["password"]);
-$phonenumber = $_POST["phone_number"];  
-$address = $_POST["address"];
-$age = $_POST["age"];*/
+}
 $user_id = $_POST["user_id"];
-echo $user_id;
-echo $fullname;
 
 $query = $mysqli->prepare("UPDATE users SET full_name= '$fullname', email='$email', password='$password', phone_number= '$phonenumber', address = '$address', age = '$age' WHERE user_id = '$user_id'");
 /*$query->bind_param("sssisi", $fullname, $email, $password, $phonenumber, $address, $age);*/
